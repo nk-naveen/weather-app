679be74a965cd818b70a69ab39fcbaa90f573ae8
naveen <nnkumar2468@gmail.com>
2022-04-25 15:06:18 +0530

    third

---
+++ assets/backgroundImages/index.js
@@ -0,0 +1,6 @@
+const haze1 = require('./img/haze1.jpg');
+const rainy = require('./img/rainy.jpg');
+const snow1 = require('./img/snow1.jpg');
+const sunny1 = require('./img/sunny1.jpg');
+
+export { haze1, rainy, snow1, sunny1 };
